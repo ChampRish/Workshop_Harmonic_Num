8ad30ce58fc15ed46f942eac4790995bc0b54229
RISHABH <gguptarishabh82@gmail.com>
2022-03-30 09:03:45 +0530

    Print the Nth Harmonic Value.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmonicNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i , n;
+            double s = 0.0;               //double used for output (output is comes in decimal values)
+            Console.Write("Enter the value: ");
+            n = int.Parse(Console.ReadLine());            
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0}+",i);   //placeholder syntax
+                s += 1 / (float)i;            // += is used for incriment plus 1
+            }
+            Console.Write("\nSum of series upto {0} /terms: {1} \n ",n,s);
+            Console.ReadLine();
+        }
+    }
+}
